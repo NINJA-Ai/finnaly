77eeeba5b482ba9f3a0321979a78cc588cb41ef5
NINJA-Ai <68237917+NINJA-Ai@users.noreply.github.com>
2021-06-29 11:19:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function start1()
+{
+    window.location = "index_tv.html";
+}
+function start2()
+{
+    window.location = "index_bed.html";
+}
+function start3()
+{
+    window.location = "index_bottle.html";
+}
+function start4()
+{
+    window.location = "index_fruit.html";
+}
+function start5()
+{
+    window.location = "index_bathroom.html";
+}
